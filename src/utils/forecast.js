a02eaafa2e4fec8b80fb0e66dd8ea46ac7ee7dcb
Mohit Nagori <nagorimohit21@gmail.com>
2019-10-11 12:31:22 +0530

    Adding temperature info in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location');
         } else {
+            const daily = body.daily.data[0];
             const data = body.currently;
-            callback(undefined, body.daily.data[0].summary + ` It is currently ${data.temperature} degrees out. There is a ${data.precipProbability}% chance of rain`);
+            callback(undefined,daily.summary + ` It is currently ${data.temperature} degrees out. This high today is ${daily.temperatureHigh} with a low of ${daily.temperatureLow}. There is a ${data.precipProbability}% chance of rain`);
         }
     });
 }
